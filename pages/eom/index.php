219a6ee9aa14b70b16ee07b8620f9eff0745a8e9
dragon1211 <inddev0807@gmail.com>
2022-01-14 04:16:49 -0800

    eom-top

---
+++ pages/eom/index.php
@@ -0,0 +1,155 @@
+
+<main class="main">
+
+<section class="eom-nav__section">
+    <div class="eom-nav__wrapper">
+        <nav class="eon-nav__first">
+            <ul><a href="<?php echo home_url(); ?>/eom">エステティックオルソメソッド</a></ul>
+        </nav>
+        <nav class="eon-nav__second">
+            <ul>
+                <li><a href="<?php echo home_url();?>/about">当院について</a></li>
+                <li><a href="<?php echo home_url();?>/about/doctor">ドクター紹介</a></li>
+                <li><a href="<?php echo home_url();?>/about/news">最新のニュース</a></li>
+            </ul>
+        </nav>
+    </div>
+</section>
+
+
+<!-- news-section -->
+<section class="news-content">
+    <div class="news-container">
+        <h2 class="news-content__caption">新着情報</h2>
+        <div class="news-wrapper">
+            <ul class="news-list">
+                <li class="news-item">
+                    <a href="<?php echo home_url();?>">
+                        <div class="news-item__image">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-common.png" alt="">
+                        </div>
+                        <div class="news-item__desc">
+                            <span class="comment">新着情報</span>
+                            <span class="date">2021年11月3日</span>
+                            <p class="title">年末年始休診日のお知らせ</p>
+                        </div>
+                    </a>
+                </li>
+                <li class="news-item">
+                    <a href="<?php echo home_url();?>">
+                        <div class="news-item__image">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-1.png" alt="">
+                        </div>
+                        <div class="news-item__desc">
+                            <span class="comment">メディア情報</span>
+                            <span class="date">2020年10月26日</span>
+                            <p class="title">10月22日発売 美的12月号「駆け込み最新デンタルケア」に当院取材記事が掲載されました</p>
+                        </div>
+                    </a>
+                </li>
+                <li class="news-item">
+                    <a href="<?php echo home_url();?>">
+                        <div class="news-item__image">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-1.png" alt="">
+                        </div>
+                        <div class="news-item__desc">
+                            <span class="comment">メディア情報</span>
+                            <span class="date">2020年10月26日</span>
+                            <p class="title">10月22日発売 美的12月号「駆け込み最新デンタルケア」に当院取材記事が掲載されました</p>
+                        </div>
+                    </a>
+                </li>
+                <li class="news-item">
+                    <a href="<?php echo home_url();?>">
+                        <div class="news-item__image">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-1.png" alt="">
+                        </div>
+                        <div class="news-item__desc">
+                            <span class="comment">メディア情報</span>
+                            <span class="date">2020年10月26日</span>
+                            <p class="title">10月22日発売 美的12月号「駆け込み最新デンタルケア」に当院取材記事が掲載されました</p>
+                        </div>
+                    </a>
+                </li>
+                <li class="news-item">
+                    <a href="<?php echo home_url();?>">
+                        <div class="news-item__image">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-common.png" alt="">
+                        </div>
+                        <div class="news-item__desc">
+                            <span class="comment">新着情報</span>
+                            <span class="date">2021年11月3日</span>
+                            <p class="title">年末年始休診日のお知らせ</p>
+                        </div>
+                    </a>
+                </li>
+                <li class="news-item">
+                    <a href="<?php echo home_url();?>">
+                        <div class="news-item__image">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-1.png" alt="">
+                        </div>
+                        <div class="news-item__desc">
+                            <span class="comment">メディア情報</span>
+                            <span class="date">2020年10月26日</span>
+                            <p class="title">10月22日発売 美的12月号「駆け込み最新デンタルケア」に当院取材記事が掲載されました</p>
+                        </div>
+                    </a>
+                </li>
+                <li class="news-item">
+                    <a href="<?php echo home_url();?>">
+                        <div class="news-item__image">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-common.png" alt="">
+                        </div>
+                        <div class="news-item__desc">
+                            <span class="comment">新着情報</span>
+                            <span class="date">2021年11月3日</span>
+                            <p class="title">年末年始休診日のお知らせ</p>
+                        </div>
+                    </a>
+                </li>
+                <li class="news-item">
+                    <a href="<?php echo home_url();?>">
+                        <div class="news-item__image">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-1.png" alt="">
+                        </div>
+                        <div class="news-item__desc">
+                            <span class="comment">メディア情報</span>
+                            <span class="date">2020年10月26日</span>
+                            <p class="title">10月22日発売 美的12月号「駆け込み最新デンタルケア」に当院取材記事が掲載されました</p>
+                        </div>
+                    </a>
+                </li>
+                <li class="news-item">
+                    <a href="<?php echo home_url();?>">
+                        <div class="news-item__image">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-common.png" alt="">
+                        </div>
+                        <div class="news-item__desc">
+                            <span class="comment">新着情報</span>
+                            <span class="date">2021年11月3日</span>
+                            <p class="title">年末年始休診日のお知らせ</p>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <a href="/??" class="news-more__link">さらに表示する </a>
+    </div>
+</section>
+
+<section class="breadcrumb-wrapper">
+    <div class="breadcrumb-wrapper__content">
+        <ul class="breadcrumb">
+            <li>
+                <a href="<?php echo home_url(); ?>">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/icon-logo-red.png" alt="logo">
+                </a>
+            </li>
+            <li><a href="<?php echo home_url(); ?>/about">当院について</a></li>
+            <li>新着情報</li>
+        </ul>
+    </div>
+</section>
+
+
+</main>
