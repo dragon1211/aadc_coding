6a768a7d8ca806738d5e9e774c56c6d7bda99780
dragon1211 <inddev0807@gmail.com>
2022-01-15 03:26:34 -0800

    completed eom

--- pages/eom/index.php
+++ pages/eom/index.php
@@ -31,124 +31,64 @@
 
 
 <!-- news-section -->
-<section class="news-content">
-    <div class="news-container">
-        <h2 class="news-content__caption">新着情報</h2>
-        <div class="news-wrapper">
-            <ul class="news-list">
-                <li class="news-item">
-                    <a href="<?php echo home_url();?>">
-                        <div class="news-item__image">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-common.png" alt="">
-                        </div>
-                        <div class="news-item__desc">
-                            <span class="comment">新着情報</span>
-                            <span class="date">2021年11月3日</span>
-                            <p class="title">年末年始休診日のお知らせ</p>
-                        </div>
-                    </a>
-                </li>
-                <li class="news-item">
-                    <a href="<?php echo home_url();?>">
-                        <div class="news-item__image">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-1.png" alt="">
-                        </div>
-                        <div class="news-item__desc">
-                            <span class="comment">メディア情報</span>
-                            <span class="date">2020年10月26日</span>
-                            <p class="title">10月22日発売 美的12月号「駆け込み最新デンタルケア」に当院取材記事が掲載されました</p>
-                        </div>
-                    </a>
-                </li>
-                <li class="news-item">
-                    <a href="<?php echo home_url();?>">
-                        <div class="news-item__image">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-1.png" alt="">
-                        </div>
-                        <div class="news-item__desc">
-                            <span class="comment">メディア情報</span>
-                            <span class="date">2020年10月26日</span>
-                            <p class="title">10月22日発売 美的12月号「駆け込み最新デンタルケア」に当院取材記事が掲載されました</p>
-                        </div>
-                    </a>
-                </li>
-                <li class="news-item">
-                    <a href="<?php echo home_url();?>">
-                        <div class="news-item__image">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-1.png" alt="">
-                        </div>
-                        <div class="news-item__desc">
-                            <span class="comment">メディア情報</span>
-                            <span class="date">2020年10月26日</span>
-                            <p class="title">10月22日発売 美的12月号「駆け込み最新デンタルケア」に当院取材記事が掲載されました</p>
-                        </div>
-                    </a>
-                </li>
-                <li class="news-item">
-                    <a href="<?php echo home_url();?>">
-                        <div class="news-item__image">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-common.png" alt="">
-                        </div>
-                        <div class="news-item__desc">
-                            <span class="comment">新着情報</span>
-                            <span class="date">2021年11月3日</span>
-                            <p class="title">年末年始休診日のお知らせ</p>
-                        </div>
-                    </a>
-                </li>
-                <li class="news-item">
-                    <a href="<?php echo home_url();?>">
-                        <div class="news-item__image">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-1.png" alt="">
-                        </div>
-                        <div class="news-item__desc">
-                            <span class="comment">メディア情報</span>
-                            <span class="date">2020年10月26日</span>
-                            <p class="title">10月22日発売 美的12月号「駆け込み最新デンタルケア」に当院取材記事が掲載されました</p>
-                        </div>
-                    </a>
-                </li>
-                <li class="news-item">
-                    <a href="<?php echo home_url();?>">
-                        <div class="news-item__image">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-common.png" alt="">
-                        </div>
-                        <div class="news-item__desc">
-                            <span class="comment">新着情報</span>
-                            <span class="date">2021年11月3日</span>
-                            <p class="title">年末年始休診日のお知らせ</p>
-                        </div>
-                    </a>
-                </li>
-                <li class="news-item">
-                    <a href="<?php echo home_url();?>">
-                        <div class="news-item__image">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-1.png" alt="">
-                        </div>
-                        <div class="news-item__desc">
-                            <span class="comment">メディア情報</span>
-                            <span class="date">2020年10月26日</span>
-                            <p class="title">10月22日発売 美的12月号「駆け込み最新デンタルケア」に当院取材記事が掲載されました</p>
-                        </div>
-                    </a>
-                </li>
-                <li class="news-item">
-                    <a href="<?php echo home_url();?>">
-                        <div class="news-item__image">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/slide-common.png" alt="">
-                        </div>
-                        <div class="news-item__desc">
-                            <span class="comment">新着情報</span>
-                            <span class="date">2021年11月3日</span>
-                            <p class="title">年末年始休診日のお知らせ</p>
-                        </div>
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <a href="/??" class="news-more__link">さらに表示する </a>
+<section class="eom-heading__content">
+    <div class="eom-heading">
+        <h2 class="eom-heading__caption">
+            <span class="jp">エステティック<br class="sp-onlyt">オルソメソッド</span>
+            <span class="en">Esthetic　Ortho　Method（EOM）</span>
+        </h2>
     </div>
 </section>
+
+<section class="eom-content">
+
+    <div class="eom-content__mainvisual">
+        <h1 class="eom-content__mainvisual--caption">
+            当院が行うエステティックオルソメソッド（EOM）とは、
+            「歯を綺麗にしたい」「口元を美しく見せたい」などのご希望や患者様の歯のお悩みに対して、
+            審美歯科.矯正歯科、噛み合わせ、それぞれの専門分野の必要な技術を融合して行う総合的な治療のことをいいます。
+        </h1>
+    </div>
+
+    <div class="eom-content__body">
+        <p class="text1">
+            当院では、一人一人の歯の状態を診断した上で、
+            患者様のご希望を踏まえEOMにより健康と美しさを備えた歯と口元を実現することを目指しています。
+            治療に入る前には、十分なカウンセリングを行い、いくつかの治療法をご提案させて頂き、
+            最善である治療方針を決定しています。
+        </p>
+        <div class="img-wrap">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_clean.png" alt="eom">
+        </div>
+        <p class="text2">
+            患者様のお悩みはそれぞれに違います。審美治療、矯正治療のどちらかのみでご希望が叶えられることもありますし、一つの治療法だけで達成できない場合は審美歯科.矯正歯科の両方の技術が必要になることもあります。<br>
+            また、どちらかの技術だけで治療を行う場合、歯を削る量が多くなる、歯肉、歯周組織の健康や審美性が損なわれる、費用負担が大きくなる、治療期間が延びるなどのデメリットが生じることもあります。<br>
+            そのようなデメリットを極力減らし、治療後の歯の健康状態の長期的安定が望め、かつ、綺麗な口元を獲得するために、それぞれの専門技術を組み合わせて行う治療が当院の考えるエステティックオルソメソッド（EOM）です。<br><br>
+
+            当院では、審美治療と矯正治療の専門的な知識と技術、経験を生かし、歯の健康と美しさを追求し細やかな部分まで配慮した総合的な治療を行なっております。<br><br>
+
+            患者様と一緒にゴールに向けて取り組み、高い満足度が得られるよう、質の高い治療を心がけています。
+        </p>
+    </div>
+
+    <div class="eom-content__menu">
+        <ul class="home-section__menu-ul">
+            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/eom/shinbi">前歯審美歯科治療</a></li>
+            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/eom/crown-bridge-inray">セラミッククラウン<br>ジルコニアクラウン</a></li>
+            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/eom/lumineers">ルミネアーズ</a></li>
+            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/eom/whitening">ホワイトニング</a></li>
+            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/eom/non-extraction">歯を抜かない矯正治療</a></li>
+            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/eom/orthodontic">前歯部分矯正</a></li>
+            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/eom/invisalign">インビザライン<br>マウスピース矯正</a></li>
+            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/eom/gap">すきっ歯専門治療</a></li>
+        </ul>
+    </div>
+
+
+
+</section>
+
+
 
 <section class="breadcrumb-wrapper">
     <div class="breadcrumb-wrapper__content">
@@ -158,11 +98,9 @@
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/icon-logo-red.png" alt="logo">
                 </a>
             </li>
-            <li><a href="<?php echo home_url(); ?>/about">当院について</a></li>
-            <li>新着情報</li>
+            <li>エステティックオルソメソッド</li>
         </ul>
     </div>
 </section>
-
 
 </main>
