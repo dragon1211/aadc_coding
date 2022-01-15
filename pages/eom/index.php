50b2257240476a9b2dd6ef497244d6271b1c1858
dragon1211 <inddev0807@gmail.com>
2022-01-15 01:25:36 -0800

    add sub-nav-menu

--- pages/eom/index.php
+++ pages/eom/index.php
@@ -1,19 +1,32 @@
 
 <main class="main">
 
-<section class="eom-nav__section">
-    <div class="eom-nav__wrapper">
-        <nav class="eon-nav__first">
-            <ul><a href="<?php echo home_url(); ?>/eom">エステティックオルソメソッド</a></ul>
-        </nav>
-        <nav class="eon-nav__second">
-            <ul>
-                <li><a href="<?php echo home_url();?>/about">当院について</a></li>
-                <li><a href="<?php echo home_url();?>/about/doctor">ドクター紹介</a></li>
-                <li><a href="<?php echo home_url();?>/about/news">最新のニュース</a></li>
+<section class="eom-nav">
+    <nav class="eom-nav__first">
+        <div class="eom-nav__container">
+            <a href="<?php echo home_url(); ?>/eom">エステティックオルソメソッド</a>
+        </div>
+    </nav>
+    <nav class="eom-nav__second">
+        <div class="eom-nav__container">
+            <div class="sp-only">
+                <div class="sp-navBtn js-sp-sub-navBtn" style="z-index: 100">
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <ul class="sub-nav__menu js-sub-nav-menu">
+                <li><a href="<?php echo home_url();?>/eom/shinbi">前歯審美歯科治療</a></li>
+                <li><a href="<?php echo home_url();?>/eom/crown-bridge-inray">セラミッククラウン<br>ジルコニアクラウン</a></li>
+                <li><a href="<?php echo home_url();?>/eom/lumineers">ルミネアーズ</a></li>
+                <li><a href="<?php echo home_url();?>/eom/whitening">ホワイトニング</a></li>
+                <li><a href="<?php echo home_url();?>/eom/non-extraction">歯を抜かない矯正治療</a></li>
+                <li><a href="<?php echo home_url();?>/eom/orthodontic">前歯部分矯正</a></li>
+                <li><a href="<?php echo home_url();?>/eom/invisalign">インビザライン<br>マウスピース矯正</a></li>
+                <li><a href="<?php echo home_url();?>/eom/gap">すきっ歯専門治療</a></li>
             </ul>
-        </nav>
-    </div>
+        </div>
+    </nav>
 </section>
 
 
