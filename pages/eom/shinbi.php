c52816676e6247c6c82c8663bb5bfaa99c941d1c
dragon1211 <inddev0807@gmail.com>
2022-01-15 15:00:20 -0800

    add shinbi

---
+++ pages/eom/shinbi.php
@@ -0,0 +1,85 @@
+
+<main class="main">
+
+<section class="eom-nav">
+    <nav class="eom-nav__first">
+        <div class="eom-nav__container">
+            <a href="<?php echo home_url(); ?>/eom">エステティックオルソメソッド</a>
+        </div>
+    </nav>
+    <nav class="eom-nav__second">
+        <div class="eom-nav__container">
+            <div class="sp-only">
+                <div class="sp-navBtn js-sp-sub-navBtn" style="z-index: 100">
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <ul class="sub-nav__menu js-sub-nav-menu">
+                <li><a href="<?php echo home_url();?>/eom/shinbi" class="active">前歯審美歯科治療</a></li>
+                <li><a href="<?php echo home_url();?>/eom/crown-bridge-inray">セラミッククラウン<br>ジルコニアクラウン</a></li>
+                <li><a href="<?php echo home_url();?>/eom/lumineers">ルミネアーズ</a></li>
+                <li><a href="<?php echo home_url();?>/eom/whitening">ホワイトニング</a></li>
+                <li><a href="<?php echo home_url();?>/eom/non-extraction">歯を抜かない矯正治療</a></li>
+                <li><a href="<?php echo home_url();?>/eom/orthodontic">前歯部分矯正</a></li>
+                <li><a href="<?php echo home_url();?>/eom/invisalign">インビザライン<br>マウスピース矯正</a></li>
+                <li><a href="<?php echo home_url();?>/eom/gap">すきっ歯専門治療</a></li>
+            </ul>
+        </div>
+    </nav>
+</section>
+
+
+<!-- news-section -->
+<section class="eom-heading__content">
+    <div class="eom-heading">
+        <h2 class="eom-heading__caption">
+            <span class="jp">前歯審美歯科治療</span>
+        </h2>
+    </div>
+</section>
+
+<section class="shinbi-content">
+
+    <div class="shinbi-content__mainvisual">
+        <div class="shinbi-content__mainvisual--image"></div>
+        <div class="shinbi-content__mainvisual--desc">
+            <h1 class="shinbi-content__mainvisual--caption">
+                美しく白い歯と<br class="sp-onlyt">きれいな歯並びを
+            </h1>
+            <p class="shinbi-content__mainvisual--text">
+                当院の審美治療は、アメリカ最新審美治療をはじめ、
+                美意識の高い患者様のニーズにもお応えできるよう、
+                高品質なセラミックと高い技術で治療を行なっています。
+                患者様一人ひとりのお口の状態やライフスタイルを考慮した
+                最善の治療法で、自信の持てる美しい口元にいたします。
+            </p>
+        </div>
+    </div>
+
+
+    <div class="shinbi-content__body">
+    
+    </div>
+
+
+
+</section>
+
+
+
+<section class="breadcrumb-wrapper">
+    <div class="breadcrumb-wrapper__content">
+        <ul class="breadcrumb">
+            <li>
+                <a href="<?php echo home_url(); ?>">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/icon-logo-red.png" alt="logo">
+                </a>
+            </li>
+            <li><a href="<?php echo home_url(); ?>/eom">エステティック<br class="sp-onlyt">オルソメソッド</a></li>
+            <li>前歯審美<br class="sp-onlyt">歯科治療</li>
+        </ul>
+    </div>
+</section>
+
+</main>
