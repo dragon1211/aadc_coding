8bb297337caea17727463ba5f2fe9e8c0f3278d1
dragon1211 <inddev0807@gmail.com>
2022-01-16 11:00:49 -0800

    complted whitening.php

---
+++ pages/eom/whitening.php
@@ -0,0 +1,187 @@
+
+<main class="main">
+
+<section class="eom-nav">
+    <nav class="eom-nav__first">
+        <div class="eom-nav__container">
+            <a href="<?php echo home_url(); ?>/eom">エステティックオルソメソッド</a>
+        </div>
+    </nav>
+    <nav class="eom-nav__second">
+        <div class="eom-nav__container">
+            <div class="sp-only">
+                <div class="sp-navBtn js-sp-sub-navBtn" style="z-index: 100">
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <ul class="sub-nav__menu js-sub-nav-menu">
+                <li><a href="<?php echo home_url();?>/eom/shinbi">前歯審美歯科治療</a></li>
+                <li><a href="<?php echo home_url();?>/eom/crown-bridge-inray">セラミッククラウン<br>ジルコニアクラウン</a></li>
+                <li><a href="<?php echo home_url();?>/eom/lumineers">ルミネアーズ</a></li>
+                <li><a href="<?php echo home_url();?>/eom/whitening" class="active">ホワイトニング</a></li>
+                <li><a href="<?php echo home_url();?>/eom/non-extraction">歯を抜かない矯正治療</a></li>
+                <li><a href="<?php echo home_url();?>/eom/orthodontic">前歯部分矯正</a></li>
+                <li><a href="<?php echo home_url();?>/eom/invisalign">インビザライン<br>マウスピース矯正</a></li>
+                <li><a href="<?php echo home_url();?>/eom/gap">すきっ歯専門治療</a></li>
+            </ul>
+        </div>
+    </nav>
+</section>
+
+
+<!---------------------------------------------------->
+<!----------------      EOM MAIN SECTION   ----------->
+<!---------------------------------------------------->
+
+
+<!-- news-section -->
+<section class="eom-heading__content">
+    <div class="eom-heading">
+        <h2 class="eom-heading__caption">
+            <span class="jp">ホワイトニング</span>
+        </h2>
+    </div>
+</section>
+
+
+<!-- shinbi-content -->
+<section class="whitening-content">
+
+	
+		<!-- outline -->
+		<section class="cure1-outline-section">
+			<h2 class="head f34">見るたびに嬉しくなる<br>くすみのない白い歯に</h2>
+			<p class="intro f18">
+				ホワイトニングは、加齢や日々の飲食によるステインが原因の<br>歯のくすみ、歯が黄色い、茶色い、などの色のお悩みを改善する事ができます。<br>
+				歯を削ることなく、ご自身の歯を白く明るくします。
+			</p>
+		</section>
+		<!-- /outline -->	
+
+
+
+		<!-- howtowhitening -->
+		<section class="cure1-howtowhitening-section f16">
+			<h2 class="head f26">当院のホワイトニング<br>治療方法</h2>
+			<h3 class="head">前歯についた汚れの除去</h3>
+			<p class="howtowhitening f14">
+				歯の表面に歯垢や歯石がある場合には、まずその汚れを取り除きます。<br>
+                （<a href="<?php echo home_url();?>/eom/????/">前歯のみのクイッククリーニングやPMTCクリーニングがおすすめです。</a>）<br>
+				表面についた汚れを落とすことで、ホワイトニング剤が歯に浸透しやすくなり、ホワイトニング効果が高くなります。また、ムラになることも防ぎます。
+			</p>
+			<h3 class="head">歯肉の保護</h3>
+			<p class="howtowhitening f14">
+				歯肉にホワイトニング剤が付着しないように、歯肉部分をブルーワセリンで保護、取れないように光で固めます。
+			</p>
+			<h3 class="head">ホワイトニング剤の塗布</h3>
+			<p class="howtowhitening f14">
+				天然歯の上下の前歯１２本と上の第一小臼歯を含めた全部で１４本の歯にホワイトニング剤を塗っていきます。（被せ物にはホワイトニング効果が出ないので、ご注意ください。）この作業を２回繰り返します。１回目で白さの度合いや知覚過敏が出ていないかなどを確認して２回目を行います。ホワイトニングの種類によって放置時間や光照射の有無が変わります。
+			</p>
+			<h3 class="head">ホワイトニング剤の除去</h3>
+			<p class="howtowhitening f14">
+				２回目が終わりましたらホワイトニング剤を完全に除去します。同時に歯肉を保護していたブルーワセリンも外していきます。
+			</p>
+			<h3 class="head">歯の保護とポリッシュでの仕上げ</h3>
+			<p class="howtowhitening f14">
+				ホワイトニング後しばらくは、外部刺激に対して歯が過敏になり、繊細な状態になるため歯を保護してあげることが必要です。
+				当院では、ホワイトニング後に歯の構成成分であるカルシウム、リンを含むミネラルペーストをパックして浸透させ歯質の強化を行います。
+				術後の知覚過敏も起こりにくくなります。<br>
+				仕上げにフッ素ジェルとシリコンの柔らかいチップで歯をポリッシングしていきます。<br>
+				この作業を行うことで、ホワイトニング後の歯をより艶やかにし、効果が持続します。
+				治療時間は初回がおおよそ１時間半、アフターホワイトニングは１時間ほどになります。
+			</p>
+			<p class="howtowhitening f14">
+				当院では、患者様ひとりひとりのご希望に添えるよう、ホワイトニング剤を３種類ご用意し、 個々の歯質や状態に合わせた、効果の出るホワイトニングをご提供できるよう努めております。</p>
+		</section>
+		<!-- /howtowhitening -->
+		
+		
+
+		<!-- office-whitening -->
+		<section class="cure1-office-whitening-section" id="office">
+			<div class="wrapper">
+                <div class="whitening-row">
+                    <div class="left">
+                        <h3 class="head f28">オフィスホワイトニング</h3>
+                        <h4 class="intro f22">即効性のあるホワイトニングで<br class="sp-onlyt">輝く白い歯に</h4>
+                        <p class="office-whitening f16">当院のホワイトニングは、アメリカデンマット社の世界初のマルチライト『サファイアハイパーライト』を使用したホワイトニングシステムです。他社のライトに比べて光がパワフルで、１回のホワイトニングで高い効果が得られます。<br>
+                            また、ホワイトニングによる知覚過敏を防ぐため鎮痛促進ジェルを使用しますので、患者様が痛みや違和感を感じることがほとんどありません。<br>
+                            さらにホワイトニング後のミネラルコーティングで歯を守りながら美しい白さを実現します。有害な紫外線も出ないので、患者様のお肌を保護する必要もありません。</p>
+                    </div>
+                    <div class="right">
+                        <img alt="オフィスホワイトニング" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/base/cure1-whitening.png" >
+                    </div>
+                </div>
+				<div class="center">
+					<h4 class="head f22">光を当てないホワイトニング <span>Opalescence Xtra Boost</span></h4>
+					<p class="xtra f14">オパールエッセンスエクストラブーストは、光照射の不要なホワイトニングシステムです。薬剤濃度が高く、
+						効果はしっかり感じられます。光過敏症の方でも安心してホワイトニングを行うことができます。
+					</p>
+					<img alt="パールエッセンスエクストラブースト" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/base/cure1-opbost.png" >
+				</div>
+				<div class="center" id="after">
+					<h4 class="head f22">アフターホワイトニング</h4>
+					<p class="xtra f14">ホワイトニングリピーターの方におすすめのコースです。<br>
+						初回ホワイトニングのアフターケアとして、また時間が経って後戻りしてしまった場合にも最適です。
+						くすみを取り、つやのある白い歯に戻します。
+						また、今より少しだけ明るくしたいなどマイルドなホワイトニングをご希望の方にもおすすめです。
+					</p>
+					<p class="comment f16">当院では、患者様ひとりひとりのご希望に添えるよう、ホワイトニング剤を３種類ご用意し、個々の歯質や状態に合わせた、効果の出るホワイトニングをご提供できるよう努めております。</p>
+				</div>
+			</div>
+		</section>
+		<!-- /office-whitening -->
+		
+		
+		
+		<!-- homee-whitening -->
+		<section class="cure1-home-whitening-section" id="home">
+            <div class="whitening-row">
+                <div class="left">
+                    <img alt="ホームホワイトニング" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/base/home-whitening.png" >
+                </div>
+                <div class="right">
+                    <h3 class="head f28">ホームホワイトニング</h3>
+                    <h4 class="intro f22">ルミブライトテイクホーム</h4>
+                    <p class="home-whitening f16">患者様がご自宅で行うホワイトニングです。1日10分、2週間の装着で9シェードのホワイトニングが可能です。濃度によって装着時間が変わります。歯の弱い方や知覚過敏を起こしやすい方は濃度の低いものをおすすめしています。サファイアハイパーライトホワイトニングのアフターケアとしても効果的です。歯形を取って専用のマウスピースを作り、その中にホワイトニングジェルを入れて行う方法です。</p>
+                </div>
+            </div>
+			<div class="annotation">
+				<h4 class="f18">ホワイトニング後の注意事項</h4>
+				<p class="f16">ホワイトニングの効果を長持ちさせるために以下のことにご注意ください。</p>
+				<ul class="f14">
+					<li>ホワイトニング後１週間は、喫煙や色の濃い食べ物や飲み物はできるだけ避けてください。飲食する場合には、すぐにお水でゆすぐか、ブラッシングを行って歯垢や色素の停滞を防いでください。</li>
+					<li>酸の強い飲食物は避けてください。（レモンやお酢など）酸が歯にダメージを与えることがあります。知覚過敏の原因になることもあります。</li>
+					<li>ホワイトニング対応の歯磨き粉を使用することをおすすめしています。<br>
+                        研磨剤の多く入ったものは表面に細かい傷がつくやすいため使用には注意してください。<br>
+                        一時的に知覚過敏が出ることがありますが、徐々に落ち着いてきますのでご安心ください。知覚過敏が強い方にはお薬や歯の保護のためにアパタイトペーストをお渡ししていますので、スタッフにお申し付けください。</li>
+				</ul>
+			</div>	
+		</section>
+		<!-- /homee-whitening -->
+
+</section>
+
+
+
+
+<!---------------------------------------------------->
+<!----------------  EOM BOTTOM BREADCRUMB  ----------->
+<!---------------------------------------------------->
+
+<section class="breadcrumb-wrapper">
+    <div class="breadcrumb-wrapper__content">
+        <ul class="breadcrumb">
+            <li>
+                <a href="<?php echo home_url(); ?>">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/icon-logo-red.png" alt="logo">
+                </a>
+            </li>
+            <li><a href="<?php echo home_url(); ?>/eom">エステティック<br class="sp-onlyt">オルソメソッド</a></li>
+            <li>ホワイトニング</li>
+        </ul>
+    </div>
+</section>
+
+</main>
