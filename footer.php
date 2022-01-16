16a4354c800858d3edde120b23ca3e1249a75be0
dragon1211 <inddev0807@gmail.com>
2022-01-15 23:33:11 -0800

    fixed files

--- footer.php
+++ footer.php
@@ -4,13 +4,12 @@
 
 </body>
   <script src="<?php echo get_template_directory_uri()?>/assets/js/libs/jquery-3.5.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/js/swiper.min.js"></script>
-  
+  <script src="<?php echo get_template_directory_uri()?>/assets/js/libs/swiper-3.4.1.min.js"></script>
   <script src="<?php echo get_template_directory_uri()?>/assets/js/libs/wow.min.js"></script>
   <script src="<?php echo get_template_directory_uri()?>/assets/js/libs/ofi.min.js"></script>
   <script src="<?php echo get_template_directory_uri()?>/assets/js/libs/slick.min.js"></script>
   <script src="<?php echo get_template_directory_uri()?>/assets/js/libs/jquery.matchHeight-min.js"></script>
-  <script src="<?php echo get_template_directory_uri()?>/assets/js/locomotive-scroll.min.js"></script>
+  <script src="<?php echo get_template_directory_uri()?>/assets/js/libs/locomotive-scroll.min.js"></script>
   <script src="<?php echo get_template_directory_uri()?>/assets/js/common.js"></script>
   <script src="<?php echo get_template_directory_uri()?>/assets/js/slider.js"></script>
 </html>
