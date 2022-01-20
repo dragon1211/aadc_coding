a8ddd28e19d120932d116ca4975f1be7e042ee5a
dragon1211 <inddev0807@gmail.com>
2022-01-20 04:51:03 -0800

    add grinding

---
+++ pages/anti-aging/grinding.php
@@ -0,0 +1,74 @@
+<?php get_template_part( 'template-parts/sub-nav/anti-aging-sub-nav' ); ?>
+
+
+<!-- anti aging section -->
+<section class="anti-heading__content">
+    <div class="anti-heading">
+        <h2 class="anti-heading__caption">
+            <span class="jp">歯ぎしり・食いしばり治療</span>
+        </h2>
+    </div>
+</section>
+
+
+<section class="content">
+    
+		<!-- outline -->
+		<section class="cure2-outline-section" id="outline">
+			<h2 class="head f36">その症状を緩和します</h2>
+			<p class="intro f18">
+				奥歯を噛みしめる癖がある、歯ぎしりで歯がすり減っている、<br>
+				顎が痛い、顎が開きにくい、首や肩がこる、などの症状を改善、予防するための治療です。</p>
+		</section>
+		<!-- /outline -->
+
+		<!-- grinding -->
+		<section class="cure2-grinding-section" id="monitor">
+			<div class="left">
+				<h3 class="head f20">マイオモニター＆プレート療法</h3>
+				<p class="grinding f16">歯や歯周組織、顎の痛みの改善や保護だけでなく、歯ぎしりやくいしばりによって起こる肩こりや首のこり、
+					頭痛などの症状を緩和して今以上に症状を悪化させないようにしていきます。<br>
+					マイオモニターを使用し、患者様一人一人にあったプレート（マウスピース）を作成します。<br>
+					就寝時に装着していただくことで、症状の緩和と改善が行えます。
+				</p>
+				<a href="<?php site_url(); ?>/about-us/equipment/" class="myomonitor-anchor m">マイオモニターの詳しい内容はこちら</a>
+				
+				<p class="grinding f16">また、噛み締めや食いしばりなどで、咬筋が過度に発達しエラが張っている方には、ボトックス注入療法も行なっております。</p>
+			</div>
+			<div class="botox f16" id="botox">
+					<h3 class="head f20">ボトックス注入療法</h3>
+					<p class="price">1シリンジ　¥44,000（税込）　技術料1回　¥3,300（税込）</p>
+					<p class="detail">
+						歯科で行うボトックス注入療法は、顎周りの筋肉（咬筋）の過度な発達と固く萎縮している状態を和らげて筋肉を緩める治療です。また、唇の上部に注入することで、ガミースマイルの改善にも効果があります。<br>
+						１回の施術で約６ヶ月ほどの効果が続きます。（個人差があります）<br>注入後数日は顎の疲労感が出る場合もありますが、徐々に消失します。</p>
+					<div class="head">ボトックス注入療法は、このような方におすすめです。</div>
+					<ul class="merit">
+						<li>強い食いしばりで顎周りの筋肉が発達してしまっている方（エラが張っている方）</li>
+						<li>寝ている時の歯ぎしりと噛み締めが原因で顎の痛みを感じる方</li>
+						<li>セラミックの詰め物やかぶせ物が治療してもすぐに割れたり欠けたりしてしまう方</li>
+						<li>噛み癖のバランスが悪く、顎周りの輪郭が左右非対称になってしまっている方</li>
+						<li>笑うと、歯茎が見えてしまう方</li>
+					</ul>
+				</div>
+
+		</section>
+		<!-- /grinding -->		
+
+</section>
+
+
+<!-- breadcrumb section -->
+<section class="breadcrumb-wrapper">
+    <div class="breadcrumb-wrapper__content">
+        <ul class="breadcrumb">
+            <li>
+                <a href="<?php echo home_url(); ?>">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/icon-logo-red.png" alt="logo">
+                </a>
+            </li>
+            <li><a href="<?php echo home_url(); ?>/anti-aging">アンチエイジング</a></li>
+            <li>歯ぎしり<br class="sp-only3"><span class="pc-only3-inline">・</span>食いしばり治療</li>
+        </ul>
+    </div>
+</section>
+<!-- breadcrumb end section -->
