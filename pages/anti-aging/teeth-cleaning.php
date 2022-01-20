467b504eed6dda68b41dbaeb942ca96aeb1b72f6
dragon1211 <inddev0807@gmail.com>
2022-01-19 20:00:05 -0800

    add teeth page

--- pages/anti-aging/teeth-cleaning.php
+++ pages/anti-aging/teeth-cleaning.php
@@ -5,56 +5,79 @@
 <section class="anti-heading__content">
     <div class="anti-heading">
         <h2 class="anti-heading__caption">
-            <span class="jp">歯科で行う<br class="sp-onlyt">アンチエイジング</span>
+            <span class="jp">クリーニング</span>
         </h2>
     </div>
 </section>
 
-<section class="anti-content">
 
-    <div class="anti-content__mainvisual">
-        <h1 class="anti-content__mainvisual--caption">
-            歯科においてのアンチエイジングでは、お口の機能を落とさずに歯を健康に保ち、
-            見た目もお口の中も若々しく保つことを目的としています。
-        </h1>
-    </div>
+<section class="content">
 
-    <div class="anti-content__body">
-        <p class="text1">
-            歯は、私たちが心身ともに健康に過ごすためにとても大切な体の一部です。毎日の生活を快適に過ごすために「歯」「口」の健康がとても重要なことをご存知でしょうか？
-            歯は、生きていく上で必要不可欠な栄養を最初に体に取り入れる大切な消化器官であり、話す、食べる、笑う、人とのコミュケーション、など毎日の生活に欠かすことのできないパーツです。
-            美味しく食事を味わい、人との関わりを積極的に行えるのも健康な歯があってこそです。
+    <!-- outline -->
+    <section class="care1-outline-section">
+        <h2 class="head f36">きれいで健康な歯と<br>歯肉のために</h2>
+        <p class="intro f18">歯を白くきれいにする、引き締まった健康な歯肉を保つ、口臭の改善予防、歯周病の改善予防、虫歯予防など、<br>
+            あなたの歯をきれいで健康な状態に保つ為のプロフェッショナルケアです。
         </p>
-        <div class="img-wrap">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/anti-aging/antiaging_smile.png" alt="antismile">
+        <p class="intro m">当院では、患者様一人一人のお口の状態を把握し、基本となる手技に加えて個々の歯や歯肉に必要な施術を行っています。最適なオーラルケアを定期的に行う事で、必ずその効果を実感頂けます。
+        </p>
+    </section>
+    <!-- /outline -->	
+  
+    <!-- teeth-cleaning -->
+    <section class="care1-teeth-cleaning-section" id="pmtc">
+        <div class="teeth-cleaning-wrap">
+            <h3 class="head f28">PMTCクリーニング</h3>
+            <p class="intro f16">歯石や歯垢、表面に付着した着色汚れやバイオフィルム、くすみを取り除き、ミネラルクリームでのパック、ポリッシュとフッ素コーティングまでを行うパーフェクトクリーニングです。艶と輝きのあるご自身本来の歯を取り戻します。敏感な口腔粘膜に優しく、殺菌効果の高い洗浄液を使用するため息もスッキリ。プロが行うオーラルケアの基本となり、口臭ケアとしても効果の高いコースです。継続することで、虫歯になりにくく、つやのある歯を保てます。 </p>
+            
+            <div class="image-wrap">
+                <div class="wrap-float">
+                    <img alt="PMTCクリーニング前" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/base/pmtc-front-before.png">
+                    <p class="arrow f20">▼</p>
+                    <img alt="PMTCクリーニング後" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/base/pmtc-front-after.png">
+                </div>
+                <div class="wrap-float">
+                    <img alt="歯クリーニング術前" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/base/pmtc-back-before.png">
+                    <p class="arrow f20">▼</p>
+                    <img alt="歯クリーニング術後" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/base/pmtc-back-after.png">
+                </div>
+            </div>
+            
+            
+            <div class="recommend">
+                <h4 class="head f20">こんな方におすすめ</h4>
+                <ul class="outline f16">
+                    <li>歯のくすみや黄ばみが気になる口臭が気になる</li>
+                    <li>虫歯や歯周病を予防したい</li>
+                    <li>唾液量が少ない</li>
+                    <li>口の中がネバネバする</li>
+                    <li>健康管理としてお手入れしたい</li>
+                    <li>たばこを吸う</li>
+                    <li>歯垢が溜まりやすい</li>
+                </ul>
+            </div>
+            
+            <div class="treatment-wrap">					
+                <h4 class="head f18">治療方法</h4>
+                <h5 class="sub-head f16">たばこやコーヒー、赤ワインなどのステインや、表面に付着したバイオフィルム（細菌の膜）を除去</h5>
+                <p class="teeth-cleaning f14">食塩と同じ成分の微粒子パウダーを歯の表面に吹き付けて、歯ブラシでは落とせない着色汚れやバイオフィルムを除去します。</p>
+                <h5 class="sub-head f16">歯石・歯垢（プラーク）など細菌のかたまりを除去</h5>
+                <p class="teeth-cleaning f14">超音波の振動で石のように硬くなった歯石を砕いて洗い流します。<br>歯肉に炎症がある場合、多少出血する場合がありますが、超音波振動とともに殺菌作用のある洗浄液が出ますので同時に消毒することができます。</p>
+                <h5 class="sub-head f16">ミネラル成分配合クリームで歯の表面の研磨</h5>
+                <p class="teeth-cleaning f14">歯質強化と汚れがつきにくくなるように歯と同じ成分のミネラル（カルシウム、リン）配合のクリームで表面を磨きパックします。</p>
+                <h5 class="sub-head f16">歯と歯の間の汚れを除去</h5>
+                <p class="teeth-cleaning f14">歯間部やその付近の歯肉を専用チップとブラシでマッサージします。<br>磨きにくいため、汚れが溜まり歯肉に炎症が出やすい部分です。歯間の汚れを取り除き、歯肉に心地良い刺激を与えて自浄作用を活性化します。</p>
+                <h5 class="sub-head f16">フッ素コーティング</h5>
+                <p class="teeth-cleaning f14">歯の表面が綺麗な状態だとフッ素の浸透がよいため効果的です。<br>フッ素塗布により虫歯になりにくい丈夫な歯にしていきます。</p>
+                <p class="comment f14">
+                    ※患者様のお口の状態に合わせて行っていくため、内容が一部変更になることもあります。
+                </p>
+            </div>
+            
+            
         </div>
-        <p class="text2">
-            噛み合わせの悪さや歯のないまま過ごすことで現れてくる顔や口の歪み、シワ、たるみ。お手入れ不足や古い治療のままの前歯によって変色した歯、、、など見た目のコンプレックスも歯が原因で引き起こされます。
-            <br>
-            <br>これほどまでに生活の質に関わってくる歯の健康をより良い状態で維持し、さらに向上させようとするのが歯科におけるアンチエイジングです。
-            <br>現在では、歯の健康を維持し口元を美しく保ち、お口の機能をより向上させるためのアンチエイジング医療が歯科分野でも浸透してきています。
-            <br>
-            <br>たとえば、皆さんがご自身のメンテナンスとして行う歯のクリーニングも、歯科でできるアンチエイジングのひとつです。定期的な歯のクリーニングは、口内環境を向上させ虫歯や歯周病になりにくい健康なお口を保つためにとても大切です。継続して行うと、お口の若返りを感じられ、老化のスピードも遅らせることができます。
-            <br>また、噛み締めや食いしばりによる咬筋（顎の筋肉）の過度な張りや硬直を緩めて症状を軽減できるボトックス注入療法や、唇のシワやほうれい線の改善、歯と歯の間の歯肉の隙間を埋めることができるヒアルロン酸注入療法も歯科分野で行えるアンチエイジングとして広まっています。
-            <br>
-            <br>そのほか、歯肉の血行を促して歯肉を引き締めたり、唾液分泌を促進させることでドライマウスにも効果的な歯肉マッサージ法や歯肉の色を綺麗にする歯肉ピーリング、唇のケアとして効果の高いリップエステ、お顔の筋肉を鍛えてシワやたるみを予防改善するフェイシャルトレーニング、舌トレーニングなど、歯科医院で行う治療と自分でできるセルフケアも含め、様々なアンチエイジング歯科治療があります。
-            <br>そのほか、歯科でのアンチエイジング医療では、歯周病の改善や全身の健康増進にも効果的な点滴療法も行っており、当クリニックではとても人気の高い治療です。
-            <br>このように、見た目を良くするだけでなく、お口周りや口腔機能をより良い状態にしてくれる歯科のアンチエイジングは、毎日を快適に過ごすためのプラスの医療として今後はもっと浸透していくでしょう。
-        </p>
-    </div>
-
-    <div class="anti-content__menu">
-        <ul class="home-section__menu-ul">
-            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/anti-aging/teeth-cleaning">クリーニング</a></li>
-            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/anti-aging/gum-care">歯周病治療<br>歯肉ケア</a></li>
-            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/anti-aging/grinding">歯ぎしり<br>食いしばり治療</a></li>
-            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/anti-aging/metal-free">メタルフリープログラム</a></li>
-            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/anti-aging/implant">インプラント</a></li>
-            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/anti-aging/anti-aging-dock">アンチエイジング検査</a></li>
-            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/anti-aging/injection">点滴療法・注射<br>サプリメント</a></li>
-            <li class="home-section__menu-ul__list"><a href="<?php echo home_url(); ?>/anti-aging/esthetic">デンタルエステ</a></li>
-        </ul>
-    </div>
+    </section>
+    <!-- /teeth-cleaning -->		
 
 </section>
 
@@ -68,7 +91,8 @@
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/icon-logo-red.png" alt="logo">
                 </a>
             </li>
-            <li>アンチエイジング</li>
+            <li><a href="<?php echo home_url(); ?>/eom">アンチエイジング</a></li>
+            <li>クリーニング</li>
         </ul>
     </div>
 </section>
