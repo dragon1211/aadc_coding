1174d2a3fadb94f90e8b52812d8d1707d443f96a
dragon1211 <inddev0807@gmail.com>
2022-01-21 03:22:48 -0800

    completed esthetic

---
+++ pages/anti-aging/esthetic.php
@@ -0,0 +1,140 @@
+<?php get_template_part( 'template-parts/sub-nav/anti-aging-sub-nav' ); ?>
+
+
+<!-- anti aging section -->
+<section class="anti-heading__content">
+    <div class="anti-heading">
+        <h2 class="anti-heading__caption">
+            <span class="jp">デンタルエステ</span>
+        </h2>
+    </div>
+</section>
+
+<!-- esthetic content -->
+<section class="content">
+
+
+		<section class="care2-outline-section">
+			<h2 class="head f28">歯だけじゃない、<br class="sp-onlyt">お口まわりをもっと美しく</h2>
+			<p class="intro f18">お口の中のお手入れだけではなく、せっかくなら唇や顎のラインもきれいにしたい、<br>
+				そんなご希望にお応えできる当院人気メニューです。<br>
+				まるでサロンのように、心地よくリラックスできます。
+			</p>
+		</section>
+
+
+		<section class="care2-esthetic-section" id="esthetic">
+			<div class="lip-esthetic f16">
+				<h3 class="head f24">リップエステ</h3>
+				<p class="price">15分 ¥2,860（税込）</p>
+				<p class="detail">皮膚の薄い唇を保護して、荒れのないつややかな状態にします。当院のリップトリートメントでは、角質柔軟成分配合のセラムでマッサージ、メディカルラインの唇専用美容液（ヒアルロン酸、レチノール配合）で保湿、ホットパックを行い、しっとり潤った血色の良い唇にしていきます。唇の乾燥や荒れ、縦じわが気になる方におすすめです。</p>
+				<h4 class="head f20">なぜ唇にもエイジングケアが<br class="sp-onlyt">必要なのでしょう?</h4>
+				<div class="why-care f16">
+					<div class="why-box">
+						<p class="dt">うるおい</p>
+						<p class="dd f16">唇には皮脂腺や汗腺が無いので、皮脂膜ができずに水分が蒸発しやすくなります。かさかさしたり荒れやすいのはその影響 です。しっかりケアすることが必要なのです。</p>
+					</div>
+					<div class="why-box">
+						<p class="dt">メラニン色素</p>
+						<p class="dd f16">唇にはメラニン色素が非常に少ないため紫外線を防ぐ事ができません。色むらや、くすみが起きやすい唇のコンディションを整 えましょう。</p>
+					</div>
+					<div class="why-box">
+						<p class="dt">角質</p>
+						<p class="dd f16">唇の角質は肌に比べてターンオーバーが早いと言われています。皮膚がめくれやすく縦じわになりやすいため、しっかり保湿 することが大切です。</p>
+					</div>
+					<div class="why-box">
+						<p class="dt">ハリ</p>
+						<p class="dd f16">唇の肌は薄く天然保湿成分が身体の肌よりきわめて少ないため、きちんと水分を保持してふっくらとしたハリを取り戻しましょう。</p>
+					</div>
+				</div>
+				<div class="practitioner-wrap f16">
+					<h4 class="head f18">施術方法</h4>
+					<img alt="リップエステ" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/base/cleansing-oil.png" >
+					<p class="dt">１．クレンジング、古い角質を除去→ダメージの修復、きめを整える</p>
+					<p class="dd">角質柔軟成分配合のセラムでマッサージ。老廃物や古くなった角質を除去し、荒れた表面をなめらかにして唇のダメージを修復。保湿成分が浸透するように唇を整えます。</p>
+					<p class="dt">２．唇専用美容液でマッサージ→血行促進、美容液成分の浸透</p>
+					<p class="dd">医療用に開発されたメディカルラインの唇専用美容液で唇をやさしくマッサージします。高保湿成分アセチルヒアルロン酸とレチノールがしっかり唇に浸透し、内側からハリと弾力を引き出します。</p>
+					<p class="dt">３．ホットパックにより保湿成分を浸透→潤いを封じ込める、保湿</p>
+					<p class="dd">マッサージ後は、さらにたっぷりの保湿ジェルを重ねてホットパックを行います。温熱刺激と蒸気、パックの心地よい重みで保湿成分を浸透させます。温熱効果で血行も促進されて唇の色が見違えるほど鮮やかになります。</p>
+					<p class="dt">４．アフタートリートメント→ふっくらハリのある唇に</p>
+					<p class="dd">ホットパック終了後は、柔らかくなった唇をやさしくマッサージします。これにより保湿効果とふっくら感が長持ちします。</p>
+				</div>
+				<p class="recom-head f20">こんな方におすすめ</p>
+				<div class="recom-wrap f16">
+					<ul class="recom">
+						<li>唇の乾燥や荒れが気になる</li>
+						<li>唇をふっくらさせたい</li>
+						<li>唇にハリと潤いが欲しい</li>
+						<li>ドライマウス</li>
+						<li>紫外線による色素沈着やくすみが気になる</li>
+						<li>唇の血色を良くしたい</li>
+						<li>唇の縦じわが気になる</li>
+					</ul>
+				</div>
+			</div>
+
+			
+			<div class="aroma f16" id="aroma">
+				<h3 class="head f20">デンタルアロマセラピー</h3>
+				<p class="price">PMTCクリーニング＋アロママッサージ<br class="sp-onlyt"> 70分 ¥13,750（税込） ／ 40分 ¥7,150</p>
+				<p class="price">ガムマッサージ＋アロママッサージ<br class="sp-onlyt"> 30分 ¥5,500（税込）</p>
+				<div class="aroma-wrap reverse">
+					<div class="left">
+						<h4 class="head f20">アロママッサージ</h4>
+						<p class="detail">
+							頭頂部から生え際、側頭部まで頭全体をマッサージした後、たるみの現れやすい顎から耳までのフェイスライン、首までをやさしくマッサージしていきます。
+							タッピングとツボ押しにより、顎のむくみがとれ、フェイスラインがすっきりし、若々しい口元をつくります。
+							顔のくすみ、たるみ、口元のしわが気になる方におすすめです。
+						</p>
+					</div>
+					<div class="right">
+						<img alt="アロママッサージ" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/base/aroma.png" >
+					</div>
+				</div>
+				<div class="aroma-wrap">
+					<div class="left">
+						<img alt="ブテイラーアロマセラピー" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/base/evetaylor.png" >
+					</div>
+					<div class="right">
+						<h4 class="head f20">イブテイラーアロマセラピー</h4>
+						<p class="detail">
+							当院では、イブテイラー自身の言葉である「アロマセラピーは人々を健やかに美しく導くこと」という考えのもとに、質の高い本物のアロマセラピーを実践してまいります。
+							症状や体調、肌、心の状態に応じて、その方の心と体が必要としているエッセンシャルオイルを選び、目の前の状況だけでなく原因や経緯をも見極めて癒しへと導いていく“トリートメント”です。
+							<br><a href="http://eve-taylor.jp/" target="_blank">詳しくは外部サイトへ</a>
+						</p>
+					</div>
+				</div>
+				<p class="comment">
+					施術は、イブテイラーデンタルアロマトリートメントの認定資格を持つ衛生士が行います。
+					主にリラクゼーションを目的としたもので、歯周病や口腔疾患への直接的な歯科治療ではありません。
+					検診や治療をご希望の方は、保険証をお持ち下さい。
+					オイルは「イブテイラー エッセンス ボディ」と「イブテイラー エッセンス フェイス」の中からお選びいただけます。
+					オイルの販売も行っております。
+				</p>
+			</div>
+			
+			
+			<div class="facial" id="facial">
+				<h3 class="head l4">表情筋マッサージ</h3>
+				<p class="price f16">20分 ¥3,300（税込）</p>
+				<p class="detail f16">マイオモニターによる機械的マッサージです。顎周りの筋肉の緊張を和らげるとともに、普段はあまり動かす事のないお顔の細かい筋肉をマイオモニターを使ってトレーニングします。目の下や頬のたるみ皺の改善に有効です。</p>
+			</div>
+		</section>
+</section>
+
+
+<!-- breadcrumb section -->
+<section class="breadcrumb-wrapper">
+    <div class="breadcrumb-wrapper__content">
+        <ul class="breadcrumb">
+            <li>
+                <a href="<?php echo home_url(); ?>">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/common/icon-logo-red.png" alt="logo">
+                </a>
+            </li>
+            <li><a href="<?php echo home_url(); ?>/anti-aging">アンチエイジング</a></li>
+            <li>デンタルエステ</li>
+        </ul>
+    </div>
+</section>
+<!-- breadcrumb end section -->
