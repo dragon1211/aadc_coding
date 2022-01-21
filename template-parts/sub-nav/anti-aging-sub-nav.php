3521b160132a8ae077cf1fb12ffc6fcec5b54388
dragon1211 <inddev0807@gmail.com>
2022-01-20 16:44:37 -0800

    fixed header sub-menu

--- template-parts/sub-nav/anti-aging-sub-nav.php
+++ template-parts/sub-nav/anti-aging-sub-nav.php
@@ -4,23 +4,17 @@
             <a href="<?php echo home_url(); ?>/anti-aging">アンチエイジング</a>
         </div>
     </nav>
-    <nav class="sub-nav__second">
+    <nav class="sub-nav__second-slide">
         <div class="sub-nav__container">
-            <div class="sp-only">
-                <div class="sp-navBtn js-sp-sub-navBtn" style="z-index: 100">
-                    <span></span>
-                    <span></span>
-                </div>
-            </div>
-            <ul class="sub-nav__menu js-sub-nav-menu">
-                <li><a href="<?php echo home_url(); ?>/anti-aging/teeth-cleaning"  class = "<?php if(strcmp(get_the_title(), "teeth-cleaning") == 0) echo "active"; ?>" >   クリーニング</a></li>
-                <li><a href="<?php echo home_url(); ?>/anti-aging/gum-care"        class = "<?php if(strcmp(get_the_title(), "gum-care") == 0) echo "active"; ?>">          歯周病治療<br><span class="sp-only-inline">・</span>歯肉ケア</a></li>
-                <li><a href="<?php echo home_url(); ?>/anti-aging/grinding"        class = "<?php if(strcmp(get_the_title(), "grinding") == 0) echo "active"; ?>">          歯ぎしり<br><span class="sp-only-inline">・</span>食いしばり治療</a></li>
-                <li><a href="<?php echo home_url(); ?>/anti-aging/metal-free"      class = "<?php if(strcmp(get_the_title(), "metal-free") == 0) echo "active"; ?>">        メタルフリープログラム</a></li>
-                <li><a href="<?php echo home_url(); ?>/anti-aging/implant"         class = "<?php if(strcmp(get_the_title(), "implant") == 0) echo "active"; ?>">           インプラント</a></li>
-                <li><a href="<?php echo home_url(); ?>/anti-aging/anti-aging-dock" class = "<?php if(strcmp(get_the_title(), "anti-aging-dock") == 0) echo "active"; ?>">   アンチエイジング検査</a></li>
-                <li><a href="<?php echo home_url(); ?>/anti-aging/injection"       class = "<?php if(strcmp(get_the_title(), "injection") == 0) echo "active"; ?>">         点滴療法・注射<br><span class="sp-only-inline">・</span>サプリメント</a></li>
-                <li><a href="<?php echo home_url(); ?>/anti-aging/esthetic"        class = "<?php if(strcmp(get_the_title(), "esthetic") == 0) echo "active"; ?>">          デンタルエステ</a></li>
+            <ul class="sub-nav__menu-slide">
+                <li><a href="<?php echo home_url(); ?>/anti-aging/teeth-cleaning"  class = "<?php if(strcmp(get_page_uri(), "anti-aging/teeth-cleaning") == 0) echo "active"; ?>" >   クリーニング</a></li>
+                <li><a href="<?php echo home_url(); ?>/anti-aging/gum-care"        class = "<?php if(strcmp(get_page_uri(), "anti-aging/gum-care") == 0) echo "active"; ?>">          歯周病治療<br>歯肉ケア</a></li>
+                <li><a href="<?php echo home_url(); ?>/anti-aging/grinding"        class = "<?php if(strcmp(get_page_uri(), "anti-aging/grinding") == 0) echo "active"; ?>">          歯ぎしり<br>食いしばり治療</a></li>
+                <li><a href="<?php echo home_url(); ?>/anti-aging/metal-free"      class = "<?php if(strcmp(get_page_uri(), "anti-aging/metal-free") == 0) echo "active"; ?>">        メタルフリープログラム</a></li>
+                <li><a href="<?php echo home_url(); ?>/anti-aging/implant"         class = "<?php if(strcmp(get_page_uri(), "anti-aging/implant") == 0) echo "active"; ?>">           インプラント</a></li>
+                <li><a href="<?php echo home_url(); ?>/anti-aging/anti-aging-dock" class = "<?php if(strcmp(get_page_uri(), "anti-aging/anti-aging-dock") == 0) echo "active"; ?>">   アンチエイジング検査</a></li>
+                <li><a href="<?php echo home_url(); ?>/anti-aging/injection"       class = "<?php if(strcmp(get_page_uri(), "anti-aging/injection") == 0) echo "active"; ?>">         点滴療法・注射<br>サプリメント</a></li>
+                <li><a href="<?php echo home_url(); ?>/anti-aging/esthetic"        class = "<?php if(strcmp(get_page_uri(), "anti-aging/esthetic") == 0) echo "active"; ?>">          デンタルエステ</a></li>
             </ul>
         </div>
     </nav>
--- template-parts/sub-nav/eom-sub-nav.php
+++ template-parts/sub-nav/eom-sub-nav.php
@@ -4,23 +4,17 @@
             <a href="<?php echo home_url(); ?>/eom">エステティックオルソメソッド</a>
         </div>
     </nav>
-    <nav class="sub-nav__second">
+    <nav class="sub-nav__second-slide">
         <div class="sub-nav__container">
-            <div class="sp-only">
-                <div class="sp-navBtn js-sp-sub-navBtn" style="z-index: 100">
-                    <span></span>
-                    <span></span>
-                </div>
-            </div>
-            <ul class="sub-nav__menu js-sub-nav-menu">
-                <li><a href="<?php echo home_url(); ?>/eom/shinbi"             class = "<?php if(strcmp(get_the_title(), "shinbi") == 0) echo "active"; ?>">            前歯審美歯科治療</a></li>
-                <li><a href="<?php echo home_url(); ?>/eom/crown-bridge-inray" class = "<?php if(strcmp(get_the_title(), "crown-bridge-inray") == 0) echo "active"; ?>">セラミッククラウン<br><span class="sp-only-inline">・</span>ジルコニアクラウン</a></li>
-                <li><a href="<?php echo home_url(); ?>/eom/lumineers"          class = "<?php if(strcmp(get_the_title(), "lumineers") == 0) echo "active"; ?>">         ルミネアーズ</a></li>
-                <li><a href="<?php echo home_url(); ?>/eom/whitening"          class = "<?php if(strcmp(get_the_title(), "whitening") == 0) echo "active"; ?>">         ホワイトニング</a></li>
-                <li><a href="<?php echo home_url(); ?>/eom/non-extraction"     class = "<?php if(strcmp(get_the_title(), "non-extraction") == 0) echo "active"; ?>">    歯を抜かない矯正治療</a></li>
-                <li><a href="<?php echo home_url(); ?>/eom/orthodontic"        class = "<?php if(strcmp(get_the_title(), "orthodontic") == 0) echo "active"; ?>">       前歯部分矯正</a></li>
-                <li><a href="<?php echo home_url(); ?>/eom/invisalign"         class = "<?php if(strcmp(get_the_title(), "invisalign") == 0) echo "active"; ?>">        インビザライン<br><span class="sp-only-inline">・</span>マウスピース矯正</a></li>
-                <li><a href="<?php echo home_url(); ?>/eom/gap"                class = "<?php if(strcmp(get_the_title(), "gap") == 0) echo "active"; ?>">               すきっ歯専門治療</a></li>
+            <ul class="sub-nav__menu-slide">
+                <li><a href="<?php echo home_url(); ?>/eom/shinbi"             class = "<?php if(strcmp(get_page_uri(), "eom/shinbi") == 0) echo "active"; ?>">            前歯審美歯科治療</a></li>
+                <li><a href="<?php echo home_url(); ?>/eom/crown-bridge-inray" class = "<?php if(strcmp(get_page_uri(), "eom/crown-bridge-inray") == 0) echo "active"; ?>">セラミッククラウン<br>ジルコニアクラウン</a></li>
+                <li><a href="<?php echo home_url(); ?>/eom/lumineers"          class = "<?php if(strcmp(get_page_uri(), "eom/lumineers") == 0) echo "active"; ?>">         ルミネアーズ</a></li>
+                <li><a href="<?php echo home_url(); ?>/eom/whitening"          class = "<?php if(strcmp(get_page_uri(), "eom/whitening") == 0) echo "active"; ?>">         ホワイトニング</a></li>
+                <li><a href="<?php echo home_url(); ?>/eom/non-extraction"     class = "<?php if(strcmp(get_page_uri(), "eom/non-extraction") == 0) echo "active"; ?>">    歯を抜かない矯正治療</a></li>
+                <li><a href="<?php echo home_url(); ?>/eom/orthodontic"        class = "<?php if(strcmp(get_page_uri(), "eom/orthodontic") == 0) echo "active"; ?>">       前歯部分矯正</a></li>
+                <li><a href="<?php echo home_url(); ?>/eom/invisalign"         class = "<?php if(strcmp(get_page_uri(), "eom/invisalign") == 0) echo "active"; ?>">        インビザライン<br>マウスピース矯正</a></li>
+                <li><a href="<?php echo home_url(); ?>/eom/gap"                class = "<?php if(strcmp(get_page_uri(), "eom/gap") == 0) echo "active"; ?>">               すきっ歯専門治療</a></li>
             </ul>
         </div>
     </nav>
