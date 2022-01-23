b80d39256dc5dbc5ad7de9846d33e1e4091e6a99
dragon1211 <inddev0807@gmail.com>
2022-01-22 18:56:17 -0800

    fixed single.php

--- single.php
+++ single.php
@@ -1,61 +1,63 @@
 <?php get_header(); ?>
 	
-	<main class="main" role="main">
-	
+<main class="main">
+
 	<!-- contents -->
-	<div class="contents">
+	<?php 
+		$post = $wp_query->post; 
 
-		<?php 
-			$post = $wp_query->post; 
-			if ( in_category(array( 'news' )) ) { 
-				get_template_part( 'template-parts/sub-nav/about-sub-nav' ); 
-			} else {
-				get_template_part( 'template-parts/sub-nav/aadcblog-sub-nav' ); 
-			} 
-			$category = get_the_category();
-			$cat_name = $category[0]->cat_name;
-			$cat_slug = $category[0]->category_nicename; 
-		?>
+		if ( in_category(array( 'news' )) ) { 
+			get_template_part( 'template-parts/sub-nav/about-sub-nav' ); 
+		} else {
+			get_template_part( 'template-parts/sub-nav/aadcblog-sub-nav' ); 
+		} 
 
-		<section class="post-news">
-			<?php if(have_posts()) : while (have_posts()) : the_post(); ?>
-				<!-- news-section -->
-				<section class="post-news__section">		
-					<div class="post-news__header">
-						<?php
-							$days = 7;
-							$today = date('U');
-							$date = get_the_time('U');
-							$period = date('U', ($today - $date)) / 86400;
-						?>
+		$category = get_the_category();
+		$cat_name = $category[0]->cat_name;
+		$cat_slug = $category[0]->category_nicename; 
+
+		$days = 7;
+		$today = date('U');
+		$date = get_the_time('U');
+		$period = date('U', ($today - $date)) / 86400;
+	?>
+
+	<section class="post">
+		<?php if(have_posts()) : while (have_posts()) : the_post(); ?>
+			<!-- news-section -->
+			<section class="post__section">		
+				<div class="post__header">
+					<p class="comment">
+						<span class="badget"><?php echo $cat_name ?></span>
 						<?php if ($days > $period){; ?>
-							<p class="badge"><span>NEW</span></p>
+							<span class="new"><span>NEW</span></span>
 						<?php }; ?>
-						<div class="date">
-							<?php echo get_the_date('Y年n月j日'); ?>
-						</div>
-						<h2 class="head f30"><?php the_title(); ?></h2>
-					</div>	
-					<div class="post-news__content">
-						<!-- <div class="thumbnail">
-							<?php 
-								if(has_post_thumbnail()){ 
-									the_post_thumbnail();
-								}; 
-							?>
-						</div> -->
-						<div class="detail f16"><?php the_content(); ?></div>
-					</div>	
-				</section>
-			<?php endwhile; endif; ?>
-		</section>
-	
+					</p>
+					<div class="date">
+						<?php echo get_the_date('Y年n月j日'); ?>
+					</div>
+					<h2 class="head f30"><?php the_title(); ?></h2>
+				</div>	
+				<div class="post__content">
+					<!-- <div class="thumbnail">
+						<?php 
+							if(has_post_thumbnail()){ 
+								the_post_thumbnail();
+							}; 
+						?>
+					</div> -->
+					<div class="detail f16"><?php the_content(); ?></div>
+				</div>	
+			</section>
+		<?php endwhile; endif; ?>
+	</section>
 
-		<!-- breadcrumb -->
-		<section class="breadcrumb-wrapper">
-			<div class="breadcrumb-wrapper__content">
-				<!-- news breadcrumb -->
-				<?php if ( in_category(array( 'news' )) ) { ?>
+
+	<!-- breadcrumb -->
+	<section class="breadcrumb-wrapper">
+		<div class="breadcrumb-wrapper__content">
+			<!-- news -->
+			<?php if ( in_category(array( 'news' )) ) { ?>
 				<ul class="breadcrumb">
 					<li>
 						<a href="<?php echo home_url(); ?>">
@@ -65,7 +67,9 @@
 					<li><a href="<?php echo home_url(); ?>/about">当院について</a></li>
 					<li><a href="<?php echo home_url()."/about/".$cat_slug; ?>"><?php echo $cat_name ?></a></li>
 				</ul>
-				<?php } else { ?>
+
+			<?php } else { ?>
+
 				<!-- aadcblog -->
 				<ul class="breadcrumb">
 					<li>
@@ -76,11 +80,10 @@
 					<li><a href="<?php echo home_url(); ?>/aadcblog">Dr.Ogawa Blog</a></li>
 					<li><a href="<?php echo home_url()."/aadcblog/".$cat_slug; ?>"><?php echo $cat_name ?></a></li>
 				</ul>
-				<?php }; ?>
-			</div>
-		</section>
+			<?php }; ?>
+		</div>
+	</section>
 
-	</div>
-
+</main>
 		
 <?php get_footer(); ?>
