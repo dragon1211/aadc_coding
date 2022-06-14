08118645938dfeeecbc23c4606591e93afb65a12
dragon1211 <vikt0r1211@hotmail.com>
2022-06-15 03:39:11 +0900

    fixed eom

--- pages/eom/index.php
+++ pages/eom/index.php
@@ -19,33 +19,313 @@
     <section class="eom-content">
         <!-- eom top mv -->
         <div class="eom-content__mainvisual">
-            <h1 class="eom-content__mainvisual--caption f18">
-                当院が行うエステティックオルソメソッド（EOM）とは、
-                「歯を綺麗にしたい」「口元を美しく見せたい」などのご希望や患者様の歯のお悩みに対して、
-                審美歯科.矯正歯科、噛み合わせ、それぞれの専門分野の必要な技術を融合して行う総合的な治療のことをいいます。
-            </h1>
+            <p class="eom-content__mainvisual--caption f18">
+                当院では、エステティックオルソメソッド（EOM）をコンセプトに治療を行っています。
+            </p>
         </div>
 
         <!-- eom top body -->
-        <div class="eom-content__body f16">
-            <p class="text1">
-                当院では、一人一人の歯の状態を診断した上で、
-                患者様のご希望を踏まえEOMにより健康と美しさを備えた歯と口元を実現することを目指しています。
-                治療に入る前には、十分なカウンセリングを行い、いくつかの治療法をご提案させて頂き、
-                最善である治療方針を決定しています。
-            </p>
+        <div class="eom-content__body">
+            <div class="txt1">
+                <h2 class="txt1-ttl">EOMとは？</h2>
+                <p>EOMとはエステティック・オルソ・メソッド(Esthetic Ortho Method)の略称で、</p>
+                <ul>
+                    <li>歯を白くしたい</li>
+                    <li>歯ならびを直したい</li>
+                    <li>歯のかたちをきれいにしたい</li>
+                </ul>
+                <p>などのご希望にお応えできるよう、審美＋矯正の専門技術を組み合わせ、</p>
+                <p>噛み合わせも考慮した治療法のことを言います。</p>
+            </div>
             <div class="img-wrap">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_clean.png" alt="eom">
             </div>
-            <p class="text2">
-                患者様のお悩みはそれぞれに違います。審美治療、矯正治療のどちらかのみでご希望が叶えられることもありますし、一つの治療法だけで達成できない場合は審美歯科.矯正歯科の両方の技術が必要になることもあります。<br>
-                また、どちらかの技術だけで治療を行う場合、歯を削る量が多くなる、歯肉、歯周組織の健康や審美性が損なわれる、費用負担が大きくなる、治療期間が延びるなどのデメリットが生じることもあります。<br>
-                そのようなデメリットを極力減らし、治療後の歯の健康状態の長期的安定が望め、かつ、綺麗な口元を獲得するために、それぞれの専門技術を組み合わせて行う治療が当院の考えるエステティックオルソメソッド（EOM）です。<br><br>
+            <div class="txt2">
+                <h2 class="txt2-ttl">EOMでもっとも大切にしていること</h2>
+                <ul>
+                    <li><span>歯へのダメージをできるだけ少なく</span></li>
+                    <li><span>治療後も、長期間良い状態を保てるように</span></li>
+                    <li><span>健康的できれいな歯、美しい口もとを実現できるように</span></li>
+                </ul>
+                <p>一人ひとりの歯のお悩みに対し、これらを<u>念頭において</u>審美＋矯正の治療プランのご提案をしています。</p>
+            </div>
+            <div class="appoint1">
+                <a href="<?php echo home_url(); ?>/appointment" class="eom-appoint__btn">
+                    診療予約・無料相談をする
+                </a>
+            </div>
+        </div>
 
-                当院では、審美治療と矯正治療の専門的な知識と技術、経験を生かし、歯の健康と美しさを追求し細やかな部分まで配慮した総合的な治療を行なっております。<br><br>
+         <!-- eom top category -->
+        <div class="eom-content__category">
+            <h2 class="ttl">EOMを用いたお悩み別、治療法例</h2>
+            <p>お悩みごとにEOMを用いた具体的な治療法についてご紹介します。</p>
+            
+            <div class="case" id="case1">
+                <h3 class="case-ttl">
+                    ケース１<br><span>前歯の凸凹と出っ歯を直したい、前歯の詰め物をきれいにしたい、歯を白くしたい</span>
+                </h3>
+                <div class="case-care__method">
+                    <h4>治療法</h4>
+                    <ul>
+                        <li>インビザライン矯正</li>
+                        <li>前歯審美歯科治療（上前歯４本のラミネートベニア法＋犬歯部分ちょっと削り）</li>
+                        <li>オフィスホワイトニング</li>
+                    </ul>
+                </div>
+                <div class="case-care__image">
+                    <p class="desc">ちょっと削りで犬歯の形を調整</p>
+                    <div class="wrap">
+                        <div class="l-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-1-1.png" alt="care-1-1">
+                            <p>Before</p>
+                        </div>
+                        <div class="arrow">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/arrow-right.svg" alt="arrow">
+                        </div>
+                        <div class="r-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-1-2.png" alt="care-1-2">
+                            <p>After</p>
+                        </div>
+                    </div>
+                    <div class="wrap">
+                        <div class="l-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-1-3.png" alt="care-1-3">
+                            <p>Before</p>
+                        </div>
+                        <div class="arrow">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/arrow-right.svg" alt="arrow">
+                        </div>
+                        <div class="r-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-1-4.png" alt="care-1-4">
+                            <p>After</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="case-ref__page">
+                    <p class="ttl">関連ページ</p>
+                    <ul class="cta-btn__bar">
+                        <li><a href="<?php echo home_url(); ?>/eom/shinbi">前歯審美歯科治療</a></li>
+                        <li><a href="<?php echo home_url(); ?>/eom/invisalign">インビザライン<br>マウスピース矯正</a></li>
+                        <li><a href="<?php echo home_url(); ?>/eom/whitening">ホワイトニング</a></li>
+                    </ul>
+                </div>
+            </div>
 
-                患者様と一緒にゴールに向けて取り組み、高い満足度が得られるよう、質の高い治療を心がけています。
+            <div class="case" id="case2">
+                <h3 class="case-ttl">
+                    ケース２<br><span>前歯の差し歯をきれいにしたい、受け口を直したい</span>
+                </h3>
+                <div class="case-care__method">
+                    <h4>治療法</h4>
+                    <ul>
+                        <li>下顎：前歯部分矯正</li>
+                        <li>上顎：前歯審美歯科治療(セラミッククラウン法)</li>
+                    </ul>
+                </div>
+                <div class="case-care__image">
+                    <p class="desc">上の前歯６本は裏側装置の部分矯正を行い、下の前歯はセラミッククラウン法にて審美治療を行った</p>
+                    <div class="wrap">
+                        <div class="l-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-2-1.png" alt="care-2-1">
+                            <p>Before</p>
+                        </div>
+                        <div class="arrow">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/arrow-right.svg" alt="arrow">
+                        </div>
+                        <div class="r-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-2-2.png" alt="care-2-2">
+                            <p>After</p>
+                        </div>
+                    </div>
+                    <div class="wrap">
+                        <div class="l-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-2-3.png" alt="care-2-3">
+                            <p>Before</p>
+                        </div>
+                        <div class="arrow">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/arrow-right.svg" alt="arrow">
+                        </div>
+                        <div class="r-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-2-4.png" alt="care-2-4">
+                            <p>After</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="case-ref__page">
+                    <p class="ttl">関連ページ</p>
+                    <ul class="cta-btn__bar">
+                        <li><a href="<?php echo home_url(); ?>/eom/orthodontic">前歯部分矯正</a></li>
+                        <li><a href="<?php echo home_url(); ?>/eom/shinbi">前歯審美歯科治療</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="case" id="case3">
+                <h3 class="case-ttl">
+                    ケース３<br><span>前歯のすき間をうめたい、歯のかたち長さを整えたい</span>
+                </h3>
+                <div class="case-care__method">
+                    <h4>治療法</h4>
+                    <ul>
+                        <li>インビザライン矯正</li>
+                        <li>前歯審美歯科治療(ダイレクトボンディング法)</li>
+                    </ul>
+                </div>
+                <div class="case-care__image">
+                    <p class="desc">上下全顎でインビザライン矯正を行い、矢印部分は、ダイレクトボンディング法で長さとかたちを<br>修正した。</p>
+                    <div class="wrap">
+                        <div class="l-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-3-1.png" alt="care-3-1">
+                            <p>Before</p>
+                        </div>
+                        <div class="arrow">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/arrow-right.svg" alt="arrow">
+                        </div>
+                        <div class="r-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-3-2.png" alt="care-3-2">
+                            <p>After</p>
+                        </div>
+                    </div>
+                    <div class="wrap">
+                        <div class="l-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-3-3.png" alt="care-3-3">
+                            <p>Before</p>
+                        </div>
+                        <div class="arrow">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/arrow-right.svg" alt="arrow">
+                        </div>
+                        <div class="r-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-3-4.png" alt="care-3-4">
+                            <p>After</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="case-ref__page">
+                    <p class="ttl">関連ページ</p>
+                    <ul class="cta-btn__bar">
+                        <li><a href="<?php echo home_url(); ?>/eom/invisalign">インビザライン<br>マウスピース矯正</a></li>
+                        <li><a href="<?php echo home_url(); ?>/eom/shinbi">前歯審美歯科治療</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="case" id="case4">
+                <h3 class="case-ttl">
+                    ケース４<br><span>前歯がガタガタで曲がっている、神経のない歯の変色を直したい</span>
+                </h3>
+                <div class="case-care__method">
+                    <h4>治療法</h4>
+                    <ul>
+                        <li>前歯部分矯正（表側ワイヤー矯正）</li>
+                        <li>前歯審美歯科治療（セラミッククラウン法）</li>
+                        <li>オフィスホワイトニング</li>
+                    </ul>
+                </div>
+                <div class="case-care__image">
+                    <p class="desc">上顎前歯は部分矯正を行い、変色している神経のない歯（左上１番）は、コアで歯を補強した後<br>セラミッククラウン法を行った</p>
+                    <div class="wrap mt-20">
+                        <div class="l-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-4-1.png" alt="care-4-1">
+                            <p>Before</p>
+                        </div>
+                        <div class="arrow">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/arrow-right.svg" alt="arrow">
+                        </div>
+                        <div class="r-wrap">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/eom/eom_case-4-2.png" alt="care-4-2">
+                            <p>After</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="case-ref__page">
+                    <p class="ttl">関連ページ</p>
+                    <ul class="cta-btn__bar">
+                        <li><a href="<?php echo home_url(); ?>/eom/orthodontic">前歯部分矯正</a></li>
+                        <li><a href="<?php echo home_url(); ?>/eom/shinbi">前歯審美歯科治療</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <!-- eom top extra -->
+        <div class="eom-content__extra">
+            <h2 class="ttl">その他の治療について</h2>
+            <p class="txt">
+                当院ではホワイトニングをはじめ、口内老化・体内老化度の検査といった治療をご用意しております。<br>
+                また、各種治療を行いながら高濃度ビタミンcやNMN点滴療法といった最先端の治療を行うことも可能です。
             </p>
+            <p>
+                当院では、患者様のご希望とお悩みのご相談を、十分なお時間を頂きカウンセリングを行っております。<br>
+                治療例以外にも、審美＋矯正の専門技術を組み合わせて、一人ひとりに満足度の高い治療方法のご提案をしております。<br>
+                カウンセリングは無料です。お気軽にご相談ください。
+            </p>
+            <div class="appoint2">
+                <a href="<?php echo home_url(); ?>/appointment" class="eom-appoint__btn">
+                    診療予約・無料相談をする
+                </a>
+            </div>
+        </div>
+
+        <!-- eom top extra -->
+        <div class="eom-content__faq">
+            <h2 class="ttl">エステティックオルソメソッドについてよくある質問</h2>
+            <ul itemscope itemtype="https://schema.org/FAQPage">
+                <li itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+                    <div itemprop="name" class="qus">エステティックオルソメソッド(EOM)とは何ですか？</div>
+                    <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                        <p itemprop="text" class="ans">
+                            エステティックオルソメソッドとは当院のコンセプトで審美＋矯正の専門技術を組み合わせ、
+                            噛み合わせも考慮した治療法を意味する造語です。
+                        </p>
+                    </div>
+                </li>
+                <li itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+                    <div itemprop="name" class="qus">エステティックオルソメソッドの治療とは？</div>
+                    <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                        <p itemprop="text" class="ans">
+                            「歯を白くしたい」「歯ならびを直したい」「歯のかたちをきれいにしたい」
+                            などのご希望にお応えできるような多角的な視点から治療を行います。
+                        </p>
+                    </div>
+                </li>
+                <li itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+                    <div itemprop="name" class="qus">エステティックオルソメソッドの治療は保険が使えますか？</div>
+                    <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                        <p itemprop="text" class="ans">
+                            虫歯があった場合など、治療内容によって保険が使えることもありますが、
+                            基本的には審美治療や矯正治療は自費になります。自費治療（セラミックや矯正など）に関しては、
+                            医療費控除の制度を受けることができます。
+                            詳しくは治療の際にお問い合わせください。
+                        </p>
+                    </div>
+                </li>
+                <li itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+                    <div itemprop="name" class="qus">歯並びだけでなく、歯の色もきれいにできますか？</div>
+                    <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                        <p itemprop="text" class="ans">
+                            はい、できます。矯正で歯並びを直した後に、
+                            歯の色やかたちの気になる部分について最適な治療をご提案しております。
+                            色のお悩みは、ホワイトニングやセラミック治療で改善できますが、
+                            歯の質や状態を診て、その方に合う治療法のご説明をさせて頂いています。
+                            エステティックオルソメソッドでは、審美＋矯正の総合的な治療を行えるので、
+                            このようなお悩みの方にぴったりの治療です。
+                        </p>
+                    </div>
+                </li>
+                <li itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+                    <div itemprop="name" class="qus">歯並び、歯の色と大きさ、全てが気になり直したいのですが、治療費用は分割でも大丈夫ですか？</div>
+                    <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                        <p itemprop="text" class="ans">
+                            お支払いについては各種クレジットカードやデンタルローンなどで負担の少ない分割払いも対応できますのでご安心ください。
+                            また、治療カウンセリングでは、治療費用のお見積もりをお渡しし、
+                            ご入金のタイミングについてもご提示させて頂いておりますので、
+                            患者様ご自身の計画に合わせてスタートすることができます。
+                            また、ご予算の範囲で行える治療法もご提案できますので、お気軽にご相談ください。
+                        </p>
+                    </div>
+                </li>
+            </ul>
         </div>
 
         <!-- eom top menu -->
